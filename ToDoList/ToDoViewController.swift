5eb50eef6d29ec55859bf59c82184e6e8b54de20
khaledmohab1 <kmohab71@yahoo.com>
2021-01-21 04:38:43 +0200

    Add/remove checkpoint on selection  of cell

--- ToDoList/ToDoViewController.swift
+++ ToDoList/ToDoViewController.swift
@@ -29,5 +29,15 @@ class ToDoViewController: UITableViewController {
        return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
